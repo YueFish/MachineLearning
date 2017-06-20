08600a4f72f37aea4cd8a744d73ed77feaef9d3b
yueyu <yue.yu@changhong.com>
2017-06-20 17:52:54 +0800

    fisrt modify

--- word2vec/word2vecDemo.py
+++ word2vec/word2vecDemo.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import jieba
+import os
 
+#读取中文文件并改变编码
+def readfile(srcfilename,desfilename):
+    f = open(srcfilename, 'r')
+    w = open(desfilename, 'w')
+    line = f.readline()
+    while line:
+        newline = line.decode('GB18030').encode('utf-8')
+        print newline,
+        print >> w, newline,
+        line = f.readline()
+    f.close()
+    w.close()
